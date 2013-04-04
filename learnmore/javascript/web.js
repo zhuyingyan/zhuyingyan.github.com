cc226c9326b489a77671515126b72aae52809831
zhuyingyan <zdsfg14@sina.com>
2013-04-04 16:32:56 +0800

    update

--- learnmore/javascript/web.js
+++ learnmore/javascript/web.js
@@ -46,4 +46,28 @@ $(function(){
 		}
 		return false;
 	});
+	$('.picWithWord img').click(function(event){
+		var bodyHeight=document.documentElement.scrollHeight,
+			clientHeight=document.documentElement.clientHeight,
+			screenHeight=bodyHeight<clientHeight?bodyHeight:clientHeight,
+			scrollTop=document.documentElement.scrollTop+document.body.scrollTop,
+			clientWidth=document.documentElement.clientWidth;	
+		bodyHeight=bodyHeight>clientHeight?bodyHeight:clientHeight;
+		$('#picShow').addClass('picBig').height(bodyHeight);
+		var $pic=$('#picShow .picDiv');
+		$('#picShow img')[0].src=event.target.src;
+		var imgTop=screenHeight/2-$pic.height()/2,
+			imgWidth=$pic.width();
+		$pic.css("margin-left",-imgWidth/2);
+		$pic.css("margin-top",imgTop+scrollTop);	
+		var bodyWidth=document.documentElement.scrollWidth;
+			bodyWidth=bodyWidth>clientWidth?bodyWidth:clientWidth,
+			bodyWidth=bodyWidth>imgWidth?bodyWidth:imgWidth;
+		$('#picShow').width(bodyWidth);
+		$('.showWord').text($(this).next('span').text());
+		return false;
+	});
+	$('.picDiv .a-closePic').click(function(){
+		$('#picShow').removeClass('picBig');
+	});
 });
